515d9dbae9ecb36d649b54bfc708a026700b4a74
Gricana <alex2904.goryachev@yandex.com>
2024-11-24 02:21:52 +0300

    Changed the storage manager class and added additional annotations to variables

--- manager/manage.py
+++ manager/manage.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 from models.book import Book
 from models.constants import BookStatus
 from models.exceptions import ValidationException, BookNotFoundException
-from storage.interface import BookStorage
+from storage.json.manager import BookStorageManager
 
 
 class BookManager:
@@ -14,14 +14,14 @@ class BookManager:
     searching and updating books in the repository.
     """
 
-    def __init__(self, book_storage: BookStorage):
+    def __init__(self, book_storage_manager: BookStorageManager):
         """
-        Initializes BookManager with a book store.
+        Initializes BookManager with a book storage manager.
 
-        :param book_storage: A book storage instance
-                             that implements the BookStorage interface.
+        :param book_storage_manager: An instance of BookStorageManager,
+                                     responsible for managing book storage.
         """
-        self.book_storage = book_storage
+        self.book_storage_manager: BookStorageManager = book_storage_manager
 
     def add_book(self, book: Book) -> None:
         """
@@ -32,7 +32,7 @@ class BookManager:
         """
         if not book.validate():
             raise ValidationException("Book data is invalid.")
-        self.book_storage.save(book)
+        self.book_storage_manager.save(book)
 
     def remove_book(self, book_id: str) -> None:
         """
@@ -41,19 +41,19 @@ class BookManager:
         :param book_id: ID of the book to delete.
         :raises BookNotFoundException: If the book with the given ID is not found.
         """
-        book = self.book_storage.get(book_id)
+        book: Optional[Book] = self.book_storage_manager.get(book_id)
         if not book:
             raise BookNotFoundException(f"Book with ID {book_id} not found")
-        self.book_storage.delete(book)
+        self.book_storage_manager.delete(book)
 
     def find_books(self, query: Optional[str] = None) -> List[Book]:
         """
-        Searches for books by title, author or year of publication.
+        Searches for books by title, author, or year of publication.
 
         :param query: Query string for searching by book fields.
         :return: List of books that satisfy the request.
         """
-        return self.book_storage.find(query)
+        return self.book_storage_manager.find(query)
 
     def update_status(self, book_id: str, status: str) -> None:
         """
@@ -64,7 +64,7 @@ class BookManager:
         :raises BookNotFoundException: If the book with the given ID is not found.
         :raises ValidationException: If an invalid status is passed.
         """
-        book = self.book_storage.get(book_id)
+        book: Optional[Book] = self.book_storage_manager.get(book_id)
         if not book:
             raise BookNotFoundException(f"Book with ID {book_id} not found")
 
@@ -72,7 +72,7 @@ class BookManager:
             raise ValidationException(f"Invalid status: {status}")
 
         book.status = status
-        self.book_storage.save(book)
+        self.book_storage_manager.save(book)
 
     def all(self) -> List[Book]:
         """
@@ -80,4 +80,4 @@ class BookManager:
 
         :return: List of all books.
         """
-        return self.book_storage.all()
+        return self.book_storage_manager.all()
